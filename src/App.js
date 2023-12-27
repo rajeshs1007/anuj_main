5761276f278e75375fee37874b1144f3651cf8ae
AllComein <144424426+AllComein@users.noreply.github.com>
2023-12-27 17:17:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css"; // Import the CSS file with the container class
 function App() {
   const link1 = "https://comparedata.vercel.app/";
   const link2 = " https://comparedynamicdata.vercel.app/";
-  // const link3 = "/link3";
+  const link3 = "https://multipleompare.vercel.app/";
 
   return (
     <div className="App">
@@ -34,19 +34,19 @@ function App() {
         </Button>
       </a>
       </section>
-      {/* <section>
-      <b><i><p>This button we can only use for the comparision</p><p>
-           of quantity difference according to the Iscin no.</p></i></b>
+       <section>
+      <b><i><p>This button we can use for the excel comparision</p><p>
+           between the PORTFOLIO and DPholding</p></i></b>
       <a href={link3} target="_blank" rel="noopener noreferrer">
         <Button
           buttonStyle="btn--aquamarine  btn--medium"
           
           type="button"
         >
-          Button 3
+          Multiple Compare
         </Button>
       </a>
-      </section> */}
+      </section> 
     </div>
   );
 }
